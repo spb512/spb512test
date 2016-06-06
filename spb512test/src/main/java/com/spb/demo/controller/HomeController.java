6954d85b585a5023a3b3b6efad008cf4e1157f07
spb512 <spb512@163.com>
2016-06-06 18:39:50 +0800

    添加shiro.jar包

--- spb512test/src/main/java/com/spb/demo/controller/HomeController.java
+++ spb512test/src/main/java/com/spb/demo/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.spb.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/home")
@@ -12,8 +17,13 @@ public class HomeController {
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping("login")
+	@RequestMapping("/login")
 	public String loginMathod(Model model ){
 		return "login";
+	}
+	@RequestMapping("/logins")
+	public String login(@RequestParam(value="name")String name,String password){
+		Subject user = SecurityUtils.getSubject();
+		return "";
 	}
 }
